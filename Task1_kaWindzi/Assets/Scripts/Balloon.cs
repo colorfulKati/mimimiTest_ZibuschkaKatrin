8c50c6016ffc320ba182ccd2bebb55a6c57c78df
Katrin-Anna Zibuschka <katrin-anna@zibuschka.com>
2020-02-29 11:58:08 +0100

    Added basic wind and balloon collision.

---
+++ Task1_kaWindzi/Assets/Scripts/Balloon.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Balloon : MonoBehaviour
+{
+	private Rigidbody m_rigidBody;
+	private Wind m_wind;
+
+	private void Awake()
+	{
+		m_rigidBody = GetComponent<Rigidbody>();
+	}
+
+	private void FixedUpdate()
+	{
+		if(m_wind != null)
+		{
+			m_rigidBody.AddForce(m_wind.v3Direction * m_wind.fStrength);
+		}
+	}
+
+	private void OnTriggerEnter(Collider _col)
+	{
+		Wind wind = _col.GetComponent<Wind>();
+
+		if(wind != null)
+		{
+			m_wind = wind;
+		}
+	}
+
+	private void OnTriggerExit(Collider _col)
+	{
+		Wind wind = _col.GetComponent<Wind>();
+
+		if (wind != null && wind == m_wind)
+		{
+			m_wind = null;
+		}
+	}
+}
---
+++ Task1_kaWindzi/Assets/Scripts/Wind.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Wind : MonoBehaviour
+{
+	[SerializeField]
+	private float m_fStrength;
+	[SerializeField]
+	private Vector3 m_v3Direction;
+
+	public float fStrength
+	{
+		get { return m_fStrength; }
+		private set { m_fStrength = value; }
+	}
+
+	public Vector3 v3Direction
+	{
+		get { return m_v3Direction; }
+		private set { m_v3Direction = value; }
+	}
+}
