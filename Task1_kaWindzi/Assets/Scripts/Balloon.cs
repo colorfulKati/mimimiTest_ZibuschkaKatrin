f0517cad7b3ac06e5afb2365d290de3dbcd2855b
Katrin-Anna Zibuschka <katrin-anna@zibuschka.com>
2020-02-29 15:54:31 +0100

    Added goal, basic resetting and lives. Also prepared stars display.

--- Task1_kaWindzi/Assets/Scripts/Balloon.cs
+++ Task1_kaWindzi/Assets/Scripts/Balloon.cs
@@ -3,11 +3,32 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Balloon : MonoBehaviour
+public class Balloon : MimiBehaviour
 {
 	[SerializeField]
 	private int m_iInitialLives;
 	private int m_iCurrentLives;
+	public int iCurrentLives
+	{
+		get { return m_iCurrentLives; }
+		private set
+		{
+			m_iCurrentLives = value;
+			OnLivesChanged?.Invoke();
+		}
+	}
+
+	private int m_iCurrentStars = 0;
+	public int iCurrentStars
+	{
+		get { return m_iCurrentStars; }
+		private set
+		{
+			m_iCurrentStars = value;
+			OnStarsChanged?.Invoke();
+		}
+	}
+
 
 	[SerializeField]
 	private float m_fDamageCooldown;
@@ -17,16 +38,29 @@ public class Balloon : MonoBehaviour
 	private Wind m_wind;
 	private WindLine m_windLine;
 
-	/// <summary>
-	/// Fired, when the balloon's number of lives changes.
-	/// Parameters: previous number of lives, current number of lives
-	/// </summary>
-	public event Action<int, int> OnLivesChanged;
+	private Vector3 m_v3InitialPosition;
 
-	private void Awake()
+	/// <summary>
+	/// Fired, when the balloon's number of lives changed.
+	/// </summary>
+	public event Action OnLivesChanged;
+
+	/// <summary>
+	/// Fired, when the number of collected stars changed.
+	/// </summary>
+	public event Action OnStarsChanged;
+
+	/// <summary>
+	/// Fired, when the balloon reached the goal.
+	/// </summary>
+	public event Action OnReachedGoal;
+
+	protected override void Awake()
 	{
+		base.Awake();
 		m_rigidBody = this.GetComponent<Rigidbody>();
 		m_iCurrentLives = m_iInitialLives;
+		m_v3InitialPosition = m_transThis.position;
 	}
 
 	private void FixedUpdate()
@@ -48,6 +82,12 @@ public class Balloon : MonoBehaviour
 		if(wind != null)
 		{
 			m_wind = wind;
+		}
+
+		Goal goal = _col.GetComponent<Goal>();
+		if(goal != null)
+		{
+			reachGoal();
 		}
 
 		//WindLine windLine = _col.GetComponent<WindLine>();
@@ -88,18 +128,35 @@ public class Balloon : MonoBehaviour
 		}
 	}
 
+	private void reset()
+	{
+		iCurrentLives = m_iInitialLives;
+		m_transThis.position = m_v3InitialPosition;
+		iCurrentStars = 0;
+	}
+
+	private void reachGoal()
+	{
+		OnReachedGoal?.Invoke();
+		Debug.Log("Balloon reached the goal.");
+
+		reset();
+	}
+
 	private void decreaseLives()
 	{
 		if (m_runningDamageCooldown != null)
 			return;
 
 		int iPrevLives = m_iCurrentLives;
-		m_iCurrentLives--;
+		iCurrentLives--;
 
-		OnLivesChanged?.Invoke(iPrevLives, m_iCurrentLives);
-		Debug.Log("Balloon lost a live. Remaining: " + m_iCurrentLives);
+		Debug.Log("Balloon lost a live. Remaining: " + iCurrentLives);
 
-		m_runningDamageCooldown = this.StartCoroutine(damageCooldown());
+		if (iCurrentLives <= 0)
+			reset();
+		else
+			m_runningDamageCooldown = this.StartCoroutine(damageCooldown());
 	}
 
 	private IEnumerator damageCooldown()
---
+++ Task1_kaWindzi/Assets/Scripts/UIManager.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIManager : MonoBehaviour
+{
+	[SerializeField]
+	private Balloon m_Balloon;
+
+	[SerializeField]
+	private Transform m_transLivesContainer;
+	[SerializeField]
+	private Transform m_transStarsContainer;
+
+	[SerializeField]
+	private GameObject m_goLifePrefab;
+	[SerializeField]
+	private GameObject m_goStarPrefab;
+
+	private void Start()
+	{
+		m_Balloon.OnLivesChanged += onLivesChanged;
+		m_Balloon.OnStarsChanged += onStarsChanged;
+
+		onLivesChanged();
+	}
+
+	private void onLivesChanged()
+	{
+		updateDisplay(m_transLivesContainer, m_goLifePrefab, m_Balloon.iCurrentLives);
+	}
+
+	private void onStarsChanged()
+	{
+		updateDisplay(m_transStarsContainer, m_goStarPrefab, m_Balloon.iCurrentStars);
+	}
+
+	private void updateDisplay(Transform _transParent, GameObject _goChildPrefab, int _iCurrentNumber)
+	{
+		int iPrevNumber = _transParent.childCount;
+
+		if (iPrevNumber > _iCurrentNumber)
+		{
+			for (int i = iPrevNumber - 1; i >= _iCurrentNumber; i--)
+			{
+				Transform transOldLife = _transParent.GetChild(i);
+				transOldLife.SetParent(null);
+				Destroy(transOldLife.gameObject);
+			}
+
+		}
+		else if (iPrevNumber < _iCurrentNumber)
+		{
+			for (int i = 0; i < _iCurrentNumber - iPrevNumber; i++)
+			{
+				GameObject goNewLife = Instantiate(_goChildPrefab, _transParent);
+			}
+		}
+	}
+}
