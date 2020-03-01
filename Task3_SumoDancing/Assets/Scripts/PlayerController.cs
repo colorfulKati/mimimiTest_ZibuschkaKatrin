83290d53fd02a427482e4a2278869c5ba72b7929
Katrin-Anna Zibuschka <katrin-anna@zibuschka.com>
2020-03-01 20:18:02 +0100

    Implemented sprite changing on player input.

--- Task3_SumoDancing/Assets/Scripts/PlayerController.cs
+++ Task3_SumoDancing/Assets/Scripts/PlayerController.cs
@@ -9,10 +9,10 @@ public class PlayerController : MonoBehaviour
 	private const int c_iFinishedIndex = -2;
 
 	/// <summary>
-	/// Raised, when a player's currently wanted key code changed.
-	/// Parameters: player index, keycode
+	/// Raised, when a player's currently wanted dance move changed.
+	/// Parameters: player index, next dance move
 	/// </summary>
-	public static event Action<int, string> OnPlayerKeyCodeChanged;
+	public static event Action<int, string> OnPlayerNextDanceMoveChanged;
 
 	/// <summary>
 	/// Raised, when a player is finished.
@@ -35,10 +35,25 @@ public class PlayerController : MonoBehaviour
 	[SerializeField]
 	private KeyCode m_KeyDown;
 
-	private Dictionary<KeyCode, DanceMoves> m_KeyToMove = new Dictionary<KeyCode, DanceMoves>();
+	[SerializeField]
+	private SpriteRenderer m_SpriteRenderer;
+
+	[SerializeField]
+	private Sprite m_SpriteLeft;
+	[SerializeField]
+	private Sprite m_SpriteRight;
+	[SerializeField]
+	private Sprite m_SpriteUp;
+	[SerializeField]
+	private Sprite m_SpriteDown;
+	[SerializeField]
+	private Sprite m_SpriteWrongMove;
+
+	private Dictionary<KeyCode, Sprite> m_KeyToSprite = new Dictionary<KeyCode, Sprite>();
 	private Dictionary<DanceMoves, KeyCode> m_MoveToKey = new Dictionary<DanceMoves, KeyCode>();
 
 	private KeyCode m_CurrentKeyCode = KeyCode.None;
+	private DanceMoves m_eCurrentDanceMove = DanceMoves.None;
 	private int m_iCurrentMoveIndex = c_iNotStartedIndex;
 	private int iCurrentMoveIndex
 	{
@@ -55,23 +70,24 @@ public class PlayerController : MonoBehaviour
 			}
 			else
 			{
-				m_CurrentKeyCode = m_MoveToKey[m_DanceSequenceGenerator.listDanceSequence[m_iCurrentMoveIndex]];
-				OnPlayerKeyCodeChanged?.Invoke(m_iPlayerIndex, m_CurrentKeyCode.ToString());
+				m_eCurrentDanceMove = m_DanceSequenceGenerator.listDanceSequence[m_iCurrentMoveIndex];
+				m_CurrentKeyCode = m_MoveToKey[m_eCurrentDanceMove];
+				OnPlayerNextDanceMoveChanged?.Invoke(m_iPlayerIndex, m_eCurrentDanceMove.ToString());
 			}
 		}
 	}
 
 	private void Start()
 	{
-		m_KeyToMove.Add(m_KeyLeft, DanceMoves.Left);
-		m_KeyToMove.Add(m_KeyRight, DanceMoves.Right);
-		m_KeyToMove.Add(m_KeyUp, DanceMoves.Up);
-		m_KeyToMove.Add(m_KeyDown, DanceMoves.Down);
-
 		m_MoveToKey.Add(DanceMoves.Left, m_KeyLeft);
 		m_MoveToKey.Add(DanceMoves.Right, m_KeyRight);
 		m_MoveToKey.Add(DanceMoves.Up, m_KeyUp);
 		m_MoveToKey.Add(DanceMoves.Down, m_KeyDown);
+
+		m_KeyToSprite.Add(m_KeyLeft, m_SpriteLeft);
+		m_KeyToSprite.Add(m_KeyRight, m_SpriteRight);
+		m_KeyToSprite.Add(m_KeyUp, m_SpriteUp);
+		m_KeyToSprite.Add(m_KeyDown, m_SpriteDown);
 	}
 
 	private void Update()
@@ -81,10 +97,12 @@ public class PlayerController : MonoBehaviour
 
 		if (Input.GetKeyDown(m_CurrentKeyCode))
 		{
+			m_SpriteRenderer.sprite = m_KeyToSprite[m_CurrentKeyCode];
 			iCurrentMoveIndex++;
 		}
 		else if (Input.GetKeyDown(m_KeyLeft) || Input.GetKeyDown(m_KeyRight) || Input.GetKeyDown(m_KeyUp) || Input.GetKeyDown(m_KeyDown))
 		{
+			m_SpriteRenderer.sprite = m_SpriteWrongMove;
 		}
 	}
 }
