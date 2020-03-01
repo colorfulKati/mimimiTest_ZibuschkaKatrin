b4f1927c8c62790589f5b3d5a2f47f30008589fe
Katrin-Anna Zibuschka <katrin-anna@zibuschka.com>
2020-03-01 12:29:33 +0100

    Create dent where rock hits terrain.

--- Task2_Volcano/Assets/Scripts/TerrainDeformation.cs
+++ Task2_Volcano/Assets/Scripts/TerrainDeformation.cs
@@ -2,20 +2,23 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class TerrainDeformation : MonoBehaviour
+public class TerrainDeformation : MimiBehaviour
 {
 	private Terrain m_Terrain;
 	private TerrainData m_TerrainData;
 
 	private float[,] m_arInitialHeightmap;
 	private int m_iHeightmapResolution;
+	private Vector3 m_v3TerrainSize;
 
-	private void Awake()
+	protected override void Awake()
 	{
+		base.Awake();
 		m_Terrain = this.GetComponent<Terrain>();
 		m_TerrainData = m_Terrain.terrainData;
 		m_iHeightmapResolution = m_TerrainData.heightmapResolution;
 		m_arInitialHeightmap = m_TerrainData.GetHeights(0, 0, m_iHeightmapResolution, m_iHeightmapResolution);
+		m_v3TerrainSize = m_TerrainData.size;
 	}
 
 	private void OnDestroy()
@@ -24,14 +27,37 @@ public class TerrainDeformation : MonoBehaviour
 			m_TerrainData.SetHeights(0, 0, m_arInitialHeightmap);
 	}
 
-	private void OnCollisionEnter(Collision _collision)
-	{
-		Debug.Log("Something collided with Terrain.");
-		
+	private void OnCollisionEnter(Collision _Collision)
+	{		
 		float[,] arHeights = m_TerrainData.GetHeights(0, 0, m_iHeightmapResolution, m_iHeightmapResolution);
+		
+		Vector3 v3LocalPoint = v3GetCollisionPointInLocalSpace(_Collision);
+		Debug.Log("Local contact point: " + v3LocalPoint);
 
-		arHeights[m_iHeightmapResolution / 2 + 10, m_iHeightmapResolution / 2 + 10] *= 0.5f;
+		Vector2Int v2HeightmapSpace = v2GetPointInHeightmapSpace(v3LocalPoint);
+		Debug.Log("Heightmap contact point: " + v2HeightmapSpace);
+
+		Debug.Log("Heightmap before: " + arHeights[v2HeightmapSpace.x, v2HeightmapSpace.y]);
+		arHeights[v2HeightmapSpace.x, v2HeightmapSpace.y] *= 0.5f;
+		Debug.Log("Heightmap after: " + arHeights[v2HeightmapSpace.x, v2HeightmapSpace.y]);
 
 		m_TerrainData.SetHeights(0, 0, arHeights);
+	}
+
+	private Vector3 v3GetCollisionPointInLocalSpace(Collision _Collision)
+	{
+		Vector3 v3World = _Collision.GetContact(0).point;
+		Debug.Log("World contact point: " + v3World);
+
+		return this.m_transThis.InverseTransformPoint(v3World);
+	}
+
+	private Vector2Int v2GetPointInHeightmapSpace(Vector3 _v3LocalPoint)
+	{
+		int iX = (int)(_v3LocalPoint.x * m_iHeightmapResolution / m_v3TerrainSize.x);
+		int iY = (int)(_v3LocalPoint.z * m_iHeightmapResolution / m_v3TerrainSize.z);
+
+		Vector2Int v2HeightmapSpace = new Vector2Int(iX, iY);
+		return v2HeightmapSpace;
 	}
 }
