83fe4f5b1aa3af658cc6f5e97a9d8fb6b605852d
Katrin-Anna Zibuschka <katrin-anna@zibuschka.com>
2020-03-01 11:53:38 +0100

    Added basic terrain deformation on collision.

---
+++ Task2_Volcano/Assets/Scripts/Rock.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rock : MonoBehaviour
+{
+	private const float c_fDestroyDelay = 2f;
+	private static WaitForSeconds s_WaitSeconds = new WaitForSeconds(c_fDestroyDelay);
+	private static WaitForEndOfFrame s_WaitEndOfFrame = new WaitForEndOfFrame();
+
+	private Collider m_collider;
+	
+	private void Awake()
+	{
+		m_collider = this.GetComponent<Collider>();
+	}
+
+	private void OnCollisionEnter(Collision collision)
+	{
+		Debug.Log("Rock collided with something.");
+
+		this.StartCoroutine(destroyDelayed());
+	}
+
+	private IEnumerator destroyDelayed()
+	{
+		m_collider.enabled = false;
+		yield return s_WaitSeconds;
+
+		Destroy(this.gameObject);
+	}
+}
---
+++ Task2_Volcano/Assets/Scripts/TerrainDeformation.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TerrainDeformation : MonoBehaviour
+{
+	private Terrain m_Terrain;
+	private TerrainData m_TerrainData;
+
+	private float[,] m_arInitialHeightmap;
+	private int m_iHeightmapResolution;
+
+	private void Awake()
+	{
+		m_Terrain = this.GetComponent<Terrain>();
+		m_TerrainData = m_Terrain.terrainData;
+		m_iHeightmapResolution = m_TerrainData.heightmapResolution;
+		m_arInitialHeightmap = m_TerrainData.GetHeights(0, 0, m_iHeightmapResolution, m_iHeightmapResolution);
+	}
+
+	private void OnDestroy()
+	{
+		if (m_TerrainData != null)
+			m_TerrainData.SetHeights(0, 0, m_arInitialHeightmap);
+	}
+
+	private void OnCollisionEnter(Collision _collision)
+	{
+		Debug.Log("Something collided with Terrain.");
+		
+		float[,] arHeights = m_TerrainData.GetHeights(0, 0, m_iHeightmapResolution, m_iHeightmapResolution);
+
+		arHeights[m_iHeightmapResolution / 2 + 10, m_iHeightmapResolution / 2 + 10] *= 0.5f;
+
+		m_TerrainData.SetHeights(0, 0, arHeights);
+	}
+}
