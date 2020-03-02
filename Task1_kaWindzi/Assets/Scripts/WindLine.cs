1030a4cd95e561a6d51bf8c07a2864bac2e05bfc
Katrin-Anna Zibuschka <katrin-anna@zibuschka.com>
2020-03-02 21:28:43 +0100

    WIP: Added wind as lineRenderer.

---
+++ Task1_kaWindzi/Assets/Scripts/WindLine.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WindLine : MimiBehaviour
+{
+	[SerializeField]
+	public Vector3 m_v3Direction;
+
+	[SerializeField]
+	public float m_fStrength;
+
+	protected override void Awake()
+	{
+		base.Awake();
+
+		LineRenderer lineRenderer = this.GetComponent<LineRenderer>();
+		MeshCollider meshCollider = this.GetComponent<MeshCollider>();
+
+		Mesh mesh = new Mesh();
+		lineRenderer.BakeMesh(mesh, true);
+		meshCollider.sharedMesh = mesh;
+	}
+}
